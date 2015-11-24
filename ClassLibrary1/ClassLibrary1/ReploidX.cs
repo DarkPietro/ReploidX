07bede5bcf0d52fc720b4b911916e2ded15b91b1
DarkPietro <darkpietrox@gmail.com>
2015-11-24 18:18:26 -0500

    initial commit

---
+++ ClassLibrary1/ClassLibrary1/ReploidX.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ff14bot.AClasses;
+using ff14bot.Managers;
+
+namespace ReploidX
+{
+
+    public class ReploidX : BotPlugin
+    {
+        public override bool WantButton
+        {
+            get { return true; }
+        }
+
+        public override string ButtonText
+        {
+            get { return "Swap"; }
+        }
+
+        public override void OnButtonPress()
+        {
+            RoutineManager.PickRoutine();
+        }
+
+        public override string Author
+        {
+            get { return " DarkPietro"; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version(0, 0, 1); }
+        }
+
+        public override string Name
+        {
+            get { return " ReploidX"; }
+        }
+
+
+    }
+
+}
